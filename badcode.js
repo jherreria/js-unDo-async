898cb9e6eb37d568f2380241dcb2c8823da2c33b
Jorge Herreria <33374603+jherreria@users.noreply.github.com>
2019-03-10 15:05:39 -0600

    Create badcode.js

---
+++ badcode.js
@@ -0,0 +1,27 @@
+//Simulates User clicking undo fast. 
+//The undo logic is async call
+
+
+function unDoPromised(id){
+  var ttt = Math.random() *3000;
+  return new Promise((resolve, reject) =>{
+    setTimeout(function restDoingUndo() {
+      console.log("done unDoing: "+id+" latency:"+ttt);
+      resolve(id+", ");
+    }, ttt);
+      
+  })
+  .then(value => {
+    msg += value;
+    $('#msg').html(msg + icon)
+  })
+}
+var msg = 'Execution order: '
+var icon = ' <i class="fa fa-smile-o"></i>'
+
+console.log(msg);
+for (var i=1;i<5;i++){
+    unDoPromised(i);
+}
+    
+//$('#msg').html(msg + icon)
